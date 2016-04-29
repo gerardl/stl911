533f34781b9e63d61de7bc29e3029e09f264285d
gerard.lucas <gerard.lucas@conperio.com>
2016-04-29 18:36:03 -0500

    parse unfriendly address into mappable address

--- Stl911Domain/ServiceCall.cs
+++ Stl911Domain/ServiceCall.cs
@@ -12,6 +12,7 @@ namespace Stl911Domain
         public string ServiceCallIdentifier { get; set; }
         public DateTime CallTime { get; set; }
         public string LocationRaw { get; set; }
+        public string LocationFriendly { get; set; }
         public string Description { get; set; }
 
         public ServiceCall() { }
@@ -22,6 +23,20 @@ namespace Stl911Domain
             CallTime = callTime;
             LocationRaw = location;
             Description = description;
+            GetFriendlyLocation();
+        }
+
+        private void GetFriendlyLocation()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(LocationRaw)) return;
+                LocationFriendly = LocationRaw.Replace("XX", "00").Replace(" / ", " and ") + ", St. Louis, MO";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
