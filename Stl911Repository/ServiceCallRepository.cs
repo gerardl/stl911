1ffdc9996e54f0edda584af2ce0a2076c3f82579
gerard.lucas <gerard.lucas@conperio.com>
2016-04-29 19:15:23 -0500

    service calls

--- Stl911Repository/ServiceCallRepository.cs
+++ Stl911Repository/ServiceCallRepository.cs
@@ -14,24 +14,47 @@ namespace Stl911Repository
         {
             try
             {
-                var serviceCalls = new List<ServiceCall>();
-
-                string Url = "http://www.slmpd.org/cfs.aspx";
-                HtmlWeb web = new HtmlWeb();
-                HtmlDocument doc = web.Load(Url);
-
-                HtmlNode table = doc.DocumentNode.SelectNodes("//*[@id=\"gvData\"]")[0];
-                foreach (HtmlNode row in table.SelectNodes("tr"))
+                using (var context = new ServiceCallContext())
                 {
-                    // Time / id / location / description
-                    var rowColumns = row.SelectNodes("td");
-                    if (rowColumns.Count != 4) continue;
+                    var serviceCalls = new List<ServiceCall>();
 
-                    var servCall = new ServiceCall(rowColumns[1].InnerHtml, DateTime.Parse(rowColumns[0].InnerHtml), rowColumns[2].InnerHtml, rowColumns[3].InnerHtml);
-                    serviceCalls.Add(servCall);
+                    string Url = "http://www.slmpd.org/cfs.aspx";
+                    HtmlWeb web = new HtmlWeb();
+                    HtmlDocument doc = web.Load(Url);
+
+                    var lastPoliceUpdate = DateTime.Parse(doc.DocumentNode.SelectNodes("//*[@id=\"lblLastUpdate\"]")[0].InnerText.Replace("Last updated: ", "").Replace(" (Central Standard Time)", ""));
+                    var lastAppUpdate = context.AppInformation.First().LastSyncTime;
+                    if (1 == 1)//(lastPoliceUpdate > lastAppUpdate)
+                    {
+                        HtmlNode table = doc.DocumentNode.SelectNodes("//*[@id=\"gvData\"]")[0];
+                        foreach (HtmlNode row in table.SelectNodes("tr"))
+                        {
+                            // Time / id / location / description
+                            var rowColumns = row.SelectNodes("td");
+                            if (rowColumns.Count != 4) continue;
+
+                            var servCall = new ServiceCall(rowColumns[1].InnerHtml, DateTime.Parse(rowColumns[0].InnerHtml), rowColumns[2].InnerHtml, rowColumns[3].InnerHtml);
+                            serviceCalls.Add(servCall);
+
+                            serviceCalls.ForEach(f =>
+                            {
+                                if (context.ServiceCall.Any(a => a.ServiceCallIdentifier == f.ServiceCallIdentifier))
+                                {
+                                    context.ServiceCall.Attach(f);
+                                    context.Entry(f).State = System.Data.Entity.EntityState.Modified;
+                                }
+                                else
+                                {
+                                    context.ServiceCall.Add(f);
+                                }
+                            });
+
+                            context.SaveChanges();
+                        }
+                    }
+
+                    return serviceCalls;
                 }
-
-                return serviceCalls;
             }
             catch (Exception)
             {
